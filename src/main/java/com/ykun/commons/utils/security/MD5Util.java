21e20883f544c8410107b0585c7d272f109f997d
decadenceqi <decadenceqi@foxmail.com>
2017-03-30 14:27:06 +0800

    1.MD5 encode

---
+++ src/main/java/com/ykun/commons/utils/security/MD5Util.java
@@ -0,0 +1,60 @@
+/*
+ * Commons-Utils
+ * Copyright (c) 2017.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ */
+
+package com.ykun.commons.utils.security;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.security.MessageDigest;
+
+/**
+ * md5
+ *
+ * @author Ykun 于 2017-03-29 16:16
+ */
+public class MD5Util {
+
+    private final static String CHAR_SET = "UTF-8";
+    private final static String DIGEST = "MD5";
+
+    /**
+     * MD5加密，32位小写
+     *
+     * @param str
+     * @return
+     */
+    public static String md5(String str) {
+        StringBuilder result = new StringBuilder();
+        try {
+            MessageDigest md5 = MessageDigest.getInstance(DIGEST);
+            md5.update(str.getBytes(CHAR_SET));
+            byte[] b = md5.digest();
+            for (int i = 0; i < b.length; ++i) {
+                int x = b[i] & 0xFF;
+                int h = x >>> 4;
+                int l = x & 0x0F;
+                result.append((char) (h + ((h < 10) ? '0' : 'a' - 10)));
+                result.append((char) (l + ((l < 10) ? '0' : 'a' - 10)));
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return result.toString();
+    }
+
+    /**
+     * 校验MD5值
+     *
+     * @param orign  未加密的字符串
+     * @param secret 比对字符串
+     * @return
+     */
+    public static boolean check(String orign, String secret) {
+        return StringUtils.equals(md5(orign), secret);
+    }
+
+}
